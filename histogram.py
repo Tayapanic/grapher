73230aec8211dc32a91041d699ee44c0e6786669
prateek <taya.prateek@gmail.com>
2017-10-21 19:47:01 +0530

    histigram

--- histogram.py
+++ histogram.py
@@ -1,7 +1,11 @@
 import matplotlib.pyplot as plt
 import pandas as pd
+from Tkinter import Tk
+from tkFileDialog import askopenfilename
 
-data = pd.read_csv('result.csv', sep=',',header=None, index_col =0)
+Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
+filename = askopenfilename()
+data = pd.read_csv(filename, sep=',',header=None, index_col =0)
 
 data.plot(kind='bar')
 plt.ylabel('Frequency')
