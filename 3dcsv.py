416701b8d634d763a636ebd7e736f525ae2156dc
RAJAT MAJOKA <rajatcse@cse.iitb.ac.in>
2017-10-22 02:05:29 +0530

    updating 3dcsv

--- 3dcsv.py
+++ 3dcsv.py
@@ -1,31 +1,18 @@
+import csv
+from matplotlib import pyplot
+import pylab
 from mpl_toolkits.mplot3d import Axes3D
-import matplotlib.pyplot as plt
-import numpy as np
 
-fig = plt.figure()
-ax = fig.gca(projection='3d')
+hFile = open("a.csv", 'r')
+datfile = csv.reader(hFile)
+dat = []
 
-# Demo 1: zdir
-zdirs = (None, 'x', 'y', 'z', (1, 1, 0), (1, 1, 1))
+for row in datfile:
+        dat.append(map(float,row))
 
-xs, ys ,zs = np.loadtxt('a.csv', unpack=True,delimiter=',')
-for zdir, x, y, z in zip(zdirs, xs, ys, zs):
-    label = '(%d, %d, %d), dir=%s' % (x, y, z, zdir)
-    ax.text(x, y, z, label, zdir)
+temp = zip(*(dat))
 
-# Demo 2: color
-ax.text(9, 0, 0, "red", color='red')
-
-# Demo 3: text2D
-# Placement 0, 0 would be the bottom left, 1, 1 would be the top right.
-ax.text2D(1, 1, "2D Text", transform=ax.transAxes)
-
-# Tweaking display region and labels
-ax.set_xlim(0, 10000)
-ax.set_ylim(0, 10000)
-ax.set_zlim(0, 10000)
-ax.set_xlabel('X axis')
-ax.set_ylabel('Y axis')
-ax.set_zlabel('Z axis')
-
-plt.show()
+fig = pylab.figure(figsize=pyplot.figaspect(.96))
+ax = Axes3D(fig)
+ax.plot_wireframe(temp[0], temp[1], temp[2])
+pyplot.show()
