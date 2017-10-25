363eebf4edb691407f2fb5141bdfdbacf4801f29
prateek <taya.prateek@gmail.com>
2017-10-25 22:19:11 +0530

    l

--- csv_data_input.py
+++ csv_data_input.py
@@ -6,12 +6,13 @@ from tkFileDialog import askopenfilename
 import csv
 import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np
+from mpl_toolkits.mplot3d import Axes3D
 #import tkMessageBox
 top=Tkinter.Tk()
 #top.geometry('816x670+0+0')
 top.title('Grapher')
 btn_font = tkFont.Font(family='Purisa')#,weight='bold')     #defining font style for buttons
-
 
 def twod_csv1():
 	filename = askopenfilename()
@@ -28,9 +29,34 @@ def twod_csv1():
 	plt.title('Interesting Graph\nCheck it out')
 	plt.legend()
 	plt.show()
-    #os.system('python 2d_csv1.py')
+def hist():
+	filename = askopenfilename()
+	data = np.loadtxt(filename)
+	plt.hist(data, normed=True, bins='auto')
+	plt.show()
+def hor_bar():
+	filename = askopenfilename()
+	data = pd.read_csv(filename, sep=',',header=None, index_col =0)
+	data.plot(kind='barh')
+	plt.ylabel('Frequency')
+	plt.xlabel('Words')
+	plt.title('Title')
+	plt.show()
 def three_d_csv():
-    os.system('python 3dcsv.py')
+    fig = plt.figure()
+    ax1 = fig.add_subplot(111,projection='3d')
+    filename = askopenfilename()
+    data = csv.reader(open(filename, 'rb'), delimiter=",", quotechar='|')
+    column1, column2,column3 = [], [],[]
+    for row in data:
+    	column1.append(float(row[0]))
+    	column2.append(float(row[1]))
+    	column3.append(float(row[2]))
+	x= np.array(column1)
+	y=np.array(column2)
+	z=np.array(column3)
+	ax1.scatter(x,y,z)
+	plt.show()
 def bar_plot():
 	filename = askopenfilename()
 	data = pd.read_csv(filename, sep=',',header=None, index_col =0)
@@ -41,20 +67,33 @@ def bar_plot():
 	plt.show()
 	os.system('python bar_graph.py')
 def pie_print():
-	os.system('python pie_data.py')
+	filename = askopenfilename()
+	data = csv.reader(open(filename, 'rb'), delimiter=",", quotechar='|')
+	column1, column2 = [], []
 
+	for row in data:
+		column1.append(row[0])
+		column2.append(row[1])
+	labels = column1
+	sizes = column2
+	fig1, ax1 = plt.subplots()
+	ax1.pie(sizes,  labels=labels, autopct='%1.1f%%',shadow=True, startangle=90)
+	ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+	plt.show()
 
 b1=Tkinter.Button(top,text="Line graph",command= twod_csv1,height=8,width=27,font=btn_font)
-b2=Tkinter.Button(top,text="Select 3d data : 3dcsv.py running",command= three_d_csv,height =8,width=27,font=btn_font)
+b2=Tkinter.Button(top,text="Horizontal Bar graph",command= hor_bar,height =8,width=27,font=btn_font)
 b3=Tkinter.Button(top,text="Bar graph",command= bar_plot,height =8,width=27,font=btn_font)
 b4=Tkinter.Button(top,text="Pie chart",command= pie_print,height =8,width=27,font=btn_font)
-
-
+b5=Tkinter.Button(top,text="Histogram",command=hist,font=btn_font,height =8,width=27)
+b6=Tkinter.Button(top,text="3d Scatter plot",command=three_d_csv,font=btn_font,height =8,width=27)
 quit_bt = Tkinter.Button(master=top, text='Quit', command=sys.exit,height =8,width=27,font=btn_font)
 
 b1.grid(row=0,column=0)
+b5.grid(row=0,column=1)
 b2.grid(row=0,column=2)
 b3.grid(row=1,column=0)
+b6.grid(row=1,column=1)
 b4.grid(row=1,column=2)
 quit_bt.grid(row=2,column=1)
 top.mainloop()
